b142bbdbeaf4fc06f93d73e63b828e3f58e232d1
Mikhail Stashko <2338881+mihsta@users.noreply.github.com>
2021-12-15 03:43:01 +0300

    update

--- iac/terraform/backend.tf
+++ iac/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraformstate"
-    storage_account_name = "__terraformstorageaccount__"
+    storage_account_name = "terrastatestorage5533"
     container_name       = "terraformstate"
     key                  = "testimport.terraform.tfstate"
   }
--- iac/terraform/terraform.tfvars
+++ iac/terraform/terraform.tfvars
@@ -1,5 +1,5 @@
 #https://www.terraform.io/docs/cli/config/environment-variables.html
 # az ad sp create-for-rbac --name "terraform_diplomapp" --skip-assignment
 #  "displayName": "terraform_diplomapp",
-appId="__TV_VAR_appid__"
-password="__TF_VAR_password__"
+appId=$(TF_VAR_appId)
+password=$(TF_VAR_password)
