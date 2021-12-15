84704fde780014549f6dab40a20d628cfcace05a
Mikhail Stashko <2338881+mihsta@users.noreply.github.com>
2021-12-15 03:30:59 +0300

    update

--- iac/terraform/backend.tf
+++ iac/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraformstate"
-    storage_account_name = "terrastatestorage5533"
+    storage_account_name = "__terraformstorageaccount__"
     container_name       = "terraformstate"
     key                  = "testimport.terraform.tfstate"
   }
