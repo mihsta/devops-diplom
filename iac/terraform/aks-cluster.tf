b2ee40e5076cca3b6549940fdecda7ec69b1c63b
Mikhail Stashko <2338881+mihsta@users.noreply.github.com>
2021-12-21 10:36:41 +0300

    up

--- iac/terraform/aks-cluster.tf
+++ iac/terraform/aks-cluster.tf
@@ -29,7 +29,7 @@ resource "azurerm_kubernetes_cluster" "default" {
 
   default_node_pool {
     name            = "default"
-    node_count      = 1
+    node_count      = 2
     vm_size         = "Standard_B2s"
     os_disk_size_gb = 30
   }
