374904389f3991a252a8cc7c9aa82b4d0e728a67
mihsta <me@stami.org>
2022-01-13 00:34:39 +0300

    update

--- diplomapp/backend/Program.cs
+++ diplomapp/backend/Program.cs
@@ -7,25 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddScoped<TrackService, TrackService>();
 
 builder.Services.AddSwaggerGen();
-
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy(name: MyAllowSpecificOrigins,
-//                      builder =>
-//                      {
-//                          builder.WithOrigins("http://localhost:5016")
-//                          .AllowAnyHeader()
-//                          .AllowAnyMethod();
-//                      });
-//});
-
-
 
 var app = builder.Build();
 
@@ -36,7 +23,6 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-//    app.UseCors(MyAllowSpecificOrigins);
 }
 
 app.UseAuthorization();
