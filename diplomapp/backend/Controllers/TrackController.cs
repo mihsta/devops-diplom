16bdb2983851aee84dc723b7f4ffc09bf20a3b90
mihsta <me@stami.org>
2022-01-13 01:22:31 +0300

    update

--- diplomapp/backend/Controllers/TrackController.cs
+++ diplomapp/backend/Controllers/TrackController.cs
@@ -26,6 +26,7 @@ namespace backend.Controllers
         [HttpGet("GetAllTracks")]
         public async Task<IEnumerable<InternalMusicTrack>> GetAllTracks(CancellationToken token)
         {
+            _trackService.DeleteAllTracks();
             return _trackService.GetAllTracksFromDb();
         }
 
